16b461d6f66df8ae8cbf89a7a2b07fecc6766422
Almaz Sharipov <alm4zch@yandex.ru>
2025-10-02 12:20:03 +0300

    added read blob data feature

--- src/handlers/blob.go
+++ src/handlers/blob.go
@@ -2,45 +2,39 @@ package handlers
 
 import (
 	"net/http"
+	"sql-proxy/src/db"
 )
 
 func ReadBlob(w http.ResponseWriter, r *http.Request) {
 
-	// if ok := checkApiVersion(w, r); !ok {
-	// 	return
-	// }
+	if ok := checkApiVersion(w, r); !ok {
+		return
+	}
 
-	// connId, sqlQuery, ok := parseQueryHttpHeadersAndBody(w, r)
-	// if !ok {
-	// 	return
-	// }
+	connId, sqlQuery, ok := parseQueryHttpHeadersAndBody(w, r)
+	if !ok {
+		return
+	}
 
-	// dbConn, ok := db.Handler.GetById(connId, true)
-	// if !ok {
-	// 	errorResponce(w, "Invalid connection id", http.StatusForbidden)
-	// 	return
-	// }
+	dbConn, ok := db.Handler.GetById(connId, true)
+	if !ok {
+		errorResponce(w, "Invalid connection id", http.StatusForbidden)
+		return
+	}
 
-	// TODO
+	var data []byte
+	err := dbConn.QueryRow(sqlQuery).Scan(&data)
+	if err != nil {
+		errorResponce(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(data)
 
 }
 
 func WriteBlob(w http.ResponseWriter, r *http.Request) {
-
-	// if ok := checkApiVersion(w, r); !ok {
-	// 	return
-	// }
-
-	// connId, sqlQuery, ok := parseQueryHttpHeadersAndBody(w, r)
-	// if !ok {
-	// 	return
-	// }
-
-	// dbConn, ok := db.Handler.GetById(connId, true)
-	// if !ok {
-	// 	errorResponce(w, "Invalid connection id", http.StatusForbidden)
-	// 	return
-	// }
 
 	// TO DO
 
